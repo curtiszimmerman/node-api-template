7ac2ba286e313b475633c6ffeac1743371f82348
curtisz <czimmerman@ezoic.com>
2014-06-08 23:55:41 -0700

    initial commit; added: pubsub object; built: base html, base css

---
+++ web.js
@@ -0,0 +1,61 @@
+/**
+ * @project node-api-template
+ * Node.js HTTP-based RESTful JSON API template
+ * @file web.js
+ * primary application driver
+ * @author curtis zimmerman
+ * @contact curtis.zimmerman@gmail.com
+ * @license GPLv2
+ * @version 0.0.1a
+ */
+
+var assert = require('assert');
+var fs = require('fs');
+var http = require('http');
+var url = require('url');
+
+/*\
+|*| pub/sub/unsub pattern utility closure
+\*/
+var _pubsub = (function() {
+	var cache = {};
+	function _flush() {
+		cache = {};
+	};
+	function _pub( topic, args, scope ) {
+		if (cache[topic]) {
+			var currentTopic = cache[topic],
+				topicLength = currentTopic.length;
+			for (var i=0; i<topicLength; i++) {
+				currentTopic[i].apply(scope || this, args || []);
+			}
+		}
+	};
+	function _sub( topic, callback ) {
+		if (!cache[topic]) {
+			cache[topic] = [];
+		}
+		cache[topic].push(callback);
+		return [topic, callback];
+	};
+	function _unsub( handle, total ) {
+		var topic = handle[0],
+			cacheLength = cache[topic].length;
+		if (cache[topic]) {
+			for (var i=0; i<cacheLength; i++) {
+				if (cache[topic][i] === handle) {
+					cache[topic].splice(cache[topic][i], 1);
+					if (total) {
+						delete cache[topic];
+					}
+				}
+			}
+		}
+	};
+	return {
+		flush: _flush,
+		pub: _pub,
+		sub: _sub,
+		unsub: _unsub
+	};
+})();
