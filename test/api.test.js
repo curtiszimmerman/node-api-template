abda346b23fd5269e63083976783ee6506c3bfdf
curtiszimmerman <curtis.zimmerman@gmail.com>
2014-11-23 23:06:05 -0800

    added: .travis.yml alt build config, tests; changed: filename

---
+++ test/api.test.js
@@ -0,0 +1,16 @@
+/**
+ * @project node-api-template
+ */
+
+var __test = (function() {
+	"use strict";
+	var expect = require('chai').expect;
+	
+	describe('node-api-template', function() {
+		describe('#api', function() {
+			it('should be grand', function() {
+				expect(true).to.equal(true);
+			});
+		});
+	});
+})();
