4ca2415276e9781ed0c130a922c6b96520cccecf
curtiszimmerman <curtis.zimmerman@gmail.com>
2016-01-13 16:34:12 -0800

    Fixed stuff

--- deploy/build.docker.sh
+++ deploy/build.docker.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
-docker build -t="node-api-template" .
+if [[ $# -ne 1 ]]; then
+	echo "You must provide the version number to build..."
+	exit 1
+fi
+
+version=$1
+
+docker build -t="node-api-template" --build-arg "VERSION=${version}" .
